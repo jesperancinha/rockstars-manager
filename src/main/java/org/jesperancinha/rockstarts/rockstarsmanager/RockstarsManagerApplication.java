6fe82ffbd6904306491c9d15b1d78e95eb5f7ef9
Joao Esperancinha <joao.esperancinha@eonics.nl>
2020-04-09 09:15:58 +0200

    Project foundations

---
+++ src/main/java/org/jesperancinha/rockstarts/rockstarsmanager/RockstarsManagerApplication.java
@@ -0,0 +1,13 @@
+package org.jesperancinha.rockstarts.rockstarsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockstarsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockstarsManagerApplication.class, args);
+	}
+
+}
