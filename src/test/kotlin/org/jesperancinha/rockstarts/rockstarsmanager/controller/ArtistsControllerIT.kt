573acc2822e78fa7f20a7cedbf21c2f0da781fd0
Joao Esperancinha <jofisaes@gmail.com>
2023-01-15 20:12:37 +0100

    Kotest tests

--- src/test/kotlin/org/jesperancinha/rockstarts/rockstarsmanager/controller/ArtistsControllerIT.kt
+++ src/test/kotlin/org/jesperancinha/rockstarts/rockstarsmanager/controller/ArtistsControllerIT.kt
@@ -1,5 +1,7 @@
 package org.jesperancinha.rockstarts.rockstarsmanager.controller
 
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 import org.assertj.core.api.Assertions
 import org.jesperancinha.rockstarts.rockstarsmanager.containers.AbstractTestContainersIT.DockerPostgresDataInitializer
 import org.jesperancinha.rockstarts.rockstarsmanager.data.ArtistDto
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.*
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.annotation.DirtiesContext.ClassMode
 import org.springframework.test.context.ActiveProfiles
@@ -22,43 +25,45 @@ import org.springframework.transaction.annotation.Transactional
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 @ContextConfiguration(initializers = [DockerPostgresDataInitializer::class])
 @Transactional
-class ArtistsControllerIT {
+class ArtistsControllerIT @Autowired constructor(
     @Autowired
-    private val controller: ArtistsController? = null
+    private val controller: ArtistsController,
+) {
+
     @Test
     fun givenArtistOnDb_whenGettingArtist_thenReturnArtistNoError() {
-        val artistsById = controller!!.getArtistsById(123L)
+        val artistsById = controller.getArtistsById(123L)
         Assertions.assertThat(artistsById).isNotNull
         Assertions.assertThat(artistsById.body).isNull()
-        Assertions.assertThat(artistsById.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+        Assertions.assertThat(artistsById.statusCode).isEqualTo(NOT_FOUND)
     }
 
     @Test
     fun givenArtistOnDb_whenGetByName_thenGetArtistByName() {
-        val artistDto: ArtistDto = ArtistDto.builder()
-            .id(10101L)
-            .name(DUA_LIPA)
-            .build()
-        val savedToDataBase = controller!!.saveArtist(artistDto)
+        val artistDto = ArtistDto(
+            id = 10101L,
+            name = DUA_LIPA
+        )
+        val savedToDataBase = controller.saveArtist(artistDto)
         val artistsByName = controller.getArtistByName(DUA_LIPA)
-        Assertions.assertThat(savedToDataBase).isNotNull
-        assertThat(savedToDataBase.getName()).isEqualTo(artistDto.getName())
-        Assertions.assertThat(artistsByName).isNotNull
-        Assertions.assertThat(artistsByName.statusCode).isEqualTo(HttpStatus.OK)
+        savedToDataBase.shouldNotBeNull()
+        savedToDataBase.name shouldBe artistDto.name
+        artistsByName.shouldNotBeNull()
+        artistsByName.statusCode shouldBe OK
         val artistsByNameBody = artistsByName.body
-        Assertions.assertThat(artistsByNameBody).isNotNull
-        assertThat(artistsByNameBody.getName()).isEqualTo(artistDto.getName())
+        artistsByNameBody.shouldNotBeNull()
+        artistsByNameBody.name shouldBe artistDto.name
     }
 
     @Test
     fun givenArtist_whenSave_thenSaveArtist() {
-        val artistDto: ArtistDto = ArtistDto.builder()
-            .id(10102L)
-            .name(ARIANA_GRANDE)
-            .build()
-        val savedToDataBase = controller!!.saveArtist(artistDto)
-        assertThat(savedToDataBase.getName()).isEqualTo(artistDto.getName())
-        assertThat(savedToDataBase.getId()).isEqualTo(1L)
+        val artistDto: ArtistDto = ArtistDto(
+            id = 10102L,
+            name = ARIANA_GRANDE)
+        val savedToDataBase = controller.saveArtist(artistDto)
+        savedToDataBase.shouldNotBeNull()
+        savedToDataBase.name shouldBe artistDto.name
+        savedToDataBase.id shouldBe 1L
     }
 
     @Test
@@ -71,7 +76,7 @@ class ArtistsControllerIT {
             .id(10101L)
             .name(MABEL)
             .build()
-        val savedAriana = controller!!.saveArtist(artistDtoAriana)
+        val savedAriana = controller.saveArtist(artistDtoAriana)
         val savedMabel = controller.saveArtist(artistDtoMabel)
         assertThat(savedAriana.getName()).isEqualTo(artistDtoAriana.getName())
         assertThat(savedMabel.getName()).isEqualTo(artistDtoMabel.getName())
@@ -83,7 +88,7 @@ class ArtistsControllerIT {
         val artistDto: ArtistDto = ArtistDto.builder()
             .name(ARIANA_GRANDE)
             .build()
-        val savedToDataBase = controller!!.saveArtist(artistDto)
+        val savedToDataBase = controller.saveArtist(artistDto)
         val artistDtoUpdate: ArtistDto = ArtistDto.builder()
             .id(savedToDataBase.getId())
             .name(MABEL)
@@ -103,13 +108,13 @@ class ArtistsControllerIT {
             .id(10102L)
             .name(ARIANA_GRANDE)
             .build()
-        val savedToDataBase = controller!!.saveArtist(artistDto)
+        val savedToDataBase = controller.saveArtist(artistDto)
         controller.deleteById(savedToDataBase.getId())
         val artistsById = controller.getArtistsById(savedToDataBase.getId())
         assertThat(savedToDataBase.getName()).isEqualTo(artistDto.getName())
         Assertions.assertThat(artistsById).isNotNull
         Assertions.assertThat(artistsById.body).isNull()
-        Assertions.assertThat(artistsById.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+        Assertions.assertThat(artistsById.statusCode).isEqualTo(NOT_FOUND)
     }
 
     companion object {
